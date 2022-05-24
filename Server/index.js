0d1b0d84993b90dad605d949a232532df20cae95
Flickza <Adelholmg@gmail.com>
2022-05-24 09:07:34 +0200

    Clean server install

---
+++ Server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+
+app.use('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const CONNECTION_URL = process.env.CONN_URL;
+const PORT = process.env.PORT;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
