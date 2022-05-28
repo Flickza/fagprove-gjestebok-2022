cc4ce93871d0de28e2fd90e6c1bb79c7e5c40a7b
Flickza <adelholmg@gmail.com>
2022-05-28 21:53:28 +0200

    tidying code, splitting templates

--- Server/app.js
+++ Server/app.js
@@ -12,26 +12,37 @@ import MongoStore from 'connect-mongo';
 import passport from 'passport';
 import "./config/passport.js";
 
+import pathfinderUI from 'pathfinder-ui';
 
 //routes
 import authRoutes from './routes/auth.js';
 import homeRoute from './routes/home.js';
 import postRoutes from './routes/posts.js';
 
+//create express app
 const app = express();
+
+//import environment variables
 dotenv.config();
+
+//set view engine to EJS
 app.set('view engine', 'ejs');
 
+//set json data limit to 50mb
 app.use(express.json({ limit: '50mb', extended: true }));
 
+//start cookie parser
 app.use(cookieParser());
 
+//urlencoded data parser
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+//set mongodb as session storage
 const sessionStore = new MongoStore({
   mongoUrl: process.env.CONN_URL, collection: 'sessions'
 });
 
+//set cookie middleware
 app.use(session({
   secret: process.env.SECRET,
   resave: false,
@@ -42,24 +53,39 @@ app.use(session({
   }
 }));
 
+//start passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-
+//set static folder location route
 app.use(express.static("./" + 'src'));
 
-
-
+//home routes
 app.use('/', homeRoute);
 
-
+//authentication routes
 app.use('/auth', authRoutes);
+
+//posts routes
 app.use('/posts', postRoutes);
 
+//templates
+app.use('/templates', express.static('./views/templates'));
 
+
+//pathfinder lib makes a visualization of API ROUTES
+//used for documentation of backend
+app.use('/pathfinder', function (req, res, next) {
+  pathfinderUI(app);
+  next();
+}, pathfinderUI.router);
+
+//set mongodb connection url and port for server to run on
 const CONNECTION_URL = process.env.CONN_URL;
 const PORT = process.env.PORT;
 
+//connect mongodb 
 mongoose.connect(CONNECTION_URL)
+  //start server if mongodb connection succeeds
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
--- Server/src/js/Index/Post.js
+++ Server/src/js/Index/Post.js
@@ -1,5 +1,5 @@
 import { paginationLogic } from "./Pagination.js";
-
+import './ejs/ejs.min.js';
 //Fetch posts from posts route
 export const fetchPosts = () => {
     //clear the messages div before fetching new messages
@@ -16,7 +16,7 @@ export const fetchPosts = () => {
 };
 
 //generate html markup for posts
-export const renderPosts = (posts, currentPage) => {
+export const renderPosts = async (posts, currentPage, done) => {
     //if there are no posts, run pagination logic with null parameters.
     //null parameter will remove pagination html
     if (!posts.length > 0) {
@@ -35,80 +35,14 @@ export const renderPosts = (posts, currentPage) => {
     //returns an array of posts
     const postsToRender = paginationLogic(posts, currentPage, limit);
 
-    let messages = postsToRender.map((message) => {
-        return `
-        <div data-id="${message._id}" class="card message mt-2 bg-light">
-                <div class="card-body d-flex flex-column flex-sm-row">
-                    <div class="cardAuthor w-10 d-flex flex-column px-5 align-items-center justify-content-center text-center border-end">
-                        <div class="authorName">
-                            <p class="fw-bold">${message.username}</p>
-                        </div>
-                        <div class="authorImage">
-                            <img src="https://avatars.githubusercontent.com/u/58791043?v=4" alt=""
-                            height="100px" width="100px" class="rounded-5 shadow">
-                        </div>
-                        <div class="userActions d-flex flex-row mt-2">
-                                <div class="edit mt-1">
-                                    <i class="bi bi-pencil-square px-1" style="font-size: 1.2rem; color: cornflowerblue;"></i>
-                                </div>
-                                <div class="delete mt-1">
-                                    <i class="bi bi-trash3 px-1" style="font-size: 1.2rem; color: cornflowerblue;"></i>
-                                </div>
-                        </div>
-                    </div>
-                    <div class="cardContent d-flex flex-column">
-                        <div class="cardTitle d-flex flex-row justify-content-between">
-                            <span class="title fw-bold mt-2 px-2">${message.title.toUpperCase()}</span>
-                            <span class="date mt-2">${message.createdAt.split("T")[0] + " " + message.createdAt.split("T")[1].split(".")[0]}</span>
-                        </div>
-                        <hr>
-                        <div class="cardBody mt-2 px-2">
-                            <span class="body">${message.body}</span>
-                        </div>
-                        <hr class="mt-1">
-                        <div class="cardFooter">
-                            <div class="actions d-flex flex-column flex-md-row align-items-center">
-                                <span class="date ms-auto text-muted fs-9">Redigert: ${message.updatedAt.split("T")[0] + " " + message.updatedAt.split("T")[1].split(".")[0]}</span>
-                            </div>
-                                <div class="commentField d-flex d-inline mb-1 mt-2 px-3">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" maxLength="50" name="commentField" placeholder="Kommentar... (Maks 50 Karakterer)">
-                                        <button type="submit" class="btn btn-primary postComment" type="button">
-                                        <i class="bi bi-send-fill" style="color:white;"></i>
-                                        </button>
-                                    </div>
-                                    <span class="quote text-end mt-1">
-                                    <i class="bi bi-chat-square-quote px-2" style="font-size: 1.5rem; color: cornflowerblue;"></i>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="children w-100 d-flex flex-column align-items-center fs-6">
-                ${message.comments.map((c) => {
-            return `
-                    <div data-id="${c._id}" class="child shadow-sm mt-2 mb-2">
-                        <div class="card d-flex flex-row align-items-center justify-content-end w-100">
-                            <div class="cardAuthor h-100 w-auto px-3 border-end">
-                                ${c.authorName}
-                            </div>
-                            <div class="card-body border-end">
-                                ${c.comment}
-                            </div>
-                            <div class="card-actions">
-                                <div class="deleteComment mt-1">
-                                    <i class="bi bi-trash3 px-1" style="font-size: 1.2rem; color: cornflowerblue;"></i>   
-                                </div>
-                            </div>
-                        </div>
-                </div>`
-        }).join("")}
-                </div>
-          </div>`;
-    }).join("");
+    //generate html markup for posts using ejs template
+    const html = await $.get("/templates/post.ejs").then(function (file, status, xhr) {
+        var htmlMarkup = ``;
+        postsToRender.forEach(post => htmlMarkup += ejs.render(file, post));
+        return htmlMarkup;
+    });
 
-    //return markup
-    return messages;
+    return html;
 }
 
 //create a new post 
@@ -126,10 +60,10 @@ export const createPost = (data) => {
 //fetch and render Posts;
 export const renderFetch = async (pagenum) => {
     //fetch all posts
-    await fetchPosts().then(function (data) {
+    await fetchPosts().then(async function (data) {
         
         //when data renderposts is called, it returns html markup
-        const html = renderPosts(data, pagenum);
+        const html = await renderPosts(data, pagenum);
         
         //append html markup to messages div
         $(".messages").html(html);
