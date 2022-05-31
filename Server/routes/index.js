a161b67b8c1cfaf617a0aef04e62fb874ffad1cb
Flickza <adelholmg@gmail.com>
2022-05-31 18:23:48 +0200

    tidy

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,16 +1,9 @@
 import express from 'express';
 
-import { alreadyAuthenticated } from '../middleware/isAuthenticated.js';
-import { loginOptionsView, loginView, registerView } from '../controllers/userViews.js';
 import { home } from '../controllers/index.js';
 const router = express.Router();
 
 //homepage
 router.get('/', home);
-
-//Authentication views
-router.get('/', alreadyAuthenticated, loginOptionsView);
-router.get('/register', alreadyAuthenticated, registerView);
-router.get('/login', alreadyAuthenticated, loginView);
 
 export default router;
